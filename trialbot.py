05c30a346c7fa9ac615b272ed263d4d7228e9d90
Madi <crandquist@gmail.com>
2020-10-02 15:19:00 -0700

    Added guild name and member welcome functions.

--- trialbot.py
+++ trialbot.py
@@ -5,11 +5,23 @@ from dotenv import load_dotenv
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
 
 client = discord.Client()
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    print(
+        f'{client.user} has connected to the following guild:\n'
+        f'{guild.name}(id: {guild.id})'
+        )
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(
+        f'Hi {member.name}, welcome to my Discord server!'
+    )
 
 client.run(TOKEN)
