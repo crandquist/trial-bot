122ea4801b156887573f5be12f0a0b6ccbc26386
Madi <crandquist@gmail.com>
2020-10-02 15:33:12 -0700

    Removed member welcome function.

--- trialbot.py
+++ trialbot.py
@@ -1,4 +1,5 @@
 import os
+import random
 
 import discord
 from dotenv import load_dotenv
@@ -12,16 +13,24 @@ client = discord.Client()
 @client.event
 async def on_ready():
     guild = discord.utils.get(client.guilds, name=GUILD)
-    print(
-        f'{client.user} has connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-        )
+    print(f'{client.user} has connected to Discord!')
 
 @client.event
-async def on_member_join(member):
-    await member.create_dm()
-    await member.dm_channel.send(
-        f'Hi {member.name}, welcome to my Discord server!'
-    )
+async def on_message(message):
+    if message.author == client.user:
+        return
+    
+    brooklyn_99_quotes = [
+        'I\'m the human form of the 💯 emoji.',
+        'Bingpot!',
+        (
+            'Cool. Cool cool cool cool cool cool cool,'
+            'no doubt no dbout no doubt no doubt.'
+        ),
+    ]
+
+    if message.content == '99!':
+        response = random.choice(brooklyn_99_quotes)
+        await message.channel.send(response)
 
 client.run(TOKEN)
